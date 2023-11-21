516a1d61087e5e4095192da220a588a2559267ff
Diego Chacon <diegdarch@hotmail.com>
2023-11-21 12:43:57 +0100

    Correcion Ej1 N2 T3

--- Tema3-Arrays/Nivel2/Ej1.php
+++ Tema3-Arrays/Nivel2/Ej1.php
@@ -1,35 +1,28 @@
-<!-- Crea un programa que contenga dos arrays de números enteros/floados. Una vez creados, muestra por pantalla el resultado de:
+<!-- Crea un programa que contenga dos arrays de números enteros/flotantes. Una vez creados, muestra por pantalla el resultado de:
 
-La intersección de los dos arrays (números en común) ❓Pregunta❓: te refieres numero enteros en comun con uno flotante. Ej: 2 = 2.0?
+La intersección de los dos arrays (números en común) 
 La mezcla de ambos arrays.
  -->
  <?php
- $arrayEnteros = [1, 6, -3, 9, 10, 189, 2, 19, -7, 89];
- $arrayFloats = [2.3, 3.6, 8.9, 9.0, 23.78, 2.00, -3.0];
+ $array1 = [1, 6, -3.7, 9, 10, 3.6, 2, 19, -7, 89];
+ $array2 = [2.3, 3.6, 8.9, 9, 23.78, 2, -3.7];
 
- function interseccionArrays($arrayEnteros, $arrayFloats): array
+ function interseccionArrays($array1, $array2): array
  {
-     $valoresComunes = [];
-      for($i=0; $i<count($arrayEnteros); $i++){
-         for($j=0; $j<count($arrayFloats); $j++){
-             if(floatval($arrayEnteros[$i]) == $arrayFloats[$j]){ //floatval convierte los enteros en numeros floats para poder comparar cada elemento
-                 array_push($valoresComunes, $arrayEnteros[$i]); //introducira los valores en comun en el array
-             }
-         }
-      }
+    $valoresComunes = array_intersect($array1, $array2);
 
       return $valoresComunes;
  }
 
- function fucionArrays($arrayEnteros, $arrayFloats): array
+ function fusionArrays($array1, $array2): array
  {  
-    $mezclaArrays = array_merge($arrayEnteros, $arrayFloats);
+    $mezclaArrays = array_merge($array1, $array2);
 
     return $mezclaArrays;
  }
 
-
 echo "Elementos en comun: ";
-print_r(interseccionArrays($arrayEnteros, $arrayFloats));
+print_r(interseccionArrays($array1, $array2));
+
 echo "<br><br> Fusion de los 2 arrays: ";
-print_r(fucionArrays($arrayEnteros, $arrayFloats));
+print_r(fusionArrays($array1, $array2));
